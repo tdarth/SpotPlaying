7ccec6aebd77835c7203816a6db9146914ce8ec6
tdarth <97992436+tdarth@users.noreply.github.com>
2025-03-26 23:45:37 -0700

    fix keybinds
    pain

--- utils/keybinds.js
+++ utils/keybinds.js
@@ -1,6 +1,10 @@
 import { Keybind } from "../../KeybindFix";
 
 import { state } from "./variables";
+import { seek } from "../utils/api/seek";
+import { setVolume } from "../utils/api/setVolume";
+import { togglePlayback } from "../utils/api/togglePlayback";
+import { navigatePlayer } from "../utils/api/navigatePlayer";
 
 new Keybind("-5 Seconds", Keyboard.KEY_NONE, "§a§lSpot§2§oPlaying").registerKeyPress(() => {
     if (state.currentSongInfo.name === "Advertisement") return showNotification(`${Settings.chatPrefix}`, `&c&l✖&r &7Wait for the &cAD&7 to finish.`, "push", 3);
